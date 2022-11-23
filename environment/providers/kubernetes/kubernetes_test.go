340450840608c6e18556d5d25247b0c9cb283b2a
Sid Shukla <sid.shukla@nutanix.com>
2022-11-23 19:11:08 +0530

    Add support for reading base64-encoded trust bundle from config map
    The base64 trust bundle configmap can be provided via both Data as well
    as BinaryData. The one in ConfigMap.Data is used as-is. The one in
    ConfigMap.BinaryData is decoded using base64.

--- environment/providers/kubernetes/kubernetes_test.go
+++ environment/providers/kubernetes/kubernetes_test.go
@@ -55,6 +55,7 @@ func runCMInformer(ctx context.Context, clientset kubernetes.Interface) coreinfo
 var _ = Describe("Kubernetes Environment Provider", Ordered, func() {
 	var (
 		secretName      = "nutanix-credentials"
+		cmName          = "user-ca-bundle"
 		secretNamespace = "kube-system"
 		secretInformer  coreinformers.SecretInformer
 		cmInformer      coreinformers.ConfigMapInformer
@@ -62,7 +63,8 @@ var _ = Describe("Kubernetes Environment Provider", Ordered, func() {
 		ip              = rand.String(10)
 		username        = rand.String(10)
 		password        = rand.String(10)
-		fakeClientset   = fake.NewSimpleClientset(&corev1.Secret{
+
+		fakeSecret = &corev1.Secret{
 			ObjectMeta: metav1.ObjectMeta{
 				Name:      secretName,
 				Namespace: secretNamespace,
@@ -83,7 +85,18 @@ var _ = Describe("Kubernetes Environment Provider", Ordered, func() {
 				]
 			`, username, password)),
 			},
-		})
+		}
+		fakeCM = &corev1.ConfigMap{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      cmName,
+				Namespace: secretNamespace,
+			},
+			BinaryData: map[string][]byte{
+				certBundleKey: []byte("LS0tLS1CRUdJTiBDRVJUSUZJQ0FURS0tLS0tCk1JSUNyRENDQVpRQ0NRQ05seHR1azgvekp6QU5CZ2txaGtpRzl3MEJBUXNGQURBWE1SVXdFd1lEVlFRRERBeGgKWkcxcGMzTnBiMjVmWTJFd0lCY05Nakl4TVRFNE1UVXdNekV4V2hnUE1qSTVOakE1TURJeE5UQXpNVEZhTUJjeApGVEFUQmdOVkJBTU1ER0ZrYldsemMybHZibDlqWVRDQ0FTSXdEUVlKS29aSWh2Y05BUUVCQlFBRGdnRVBBRENDCkFRb0NnZ0VCQUtyTTZ4VndlbGppN29hNThoc0w3UERZS0E4cVJ4THFYc2JBQ3pIdXZHcUVUQktmU0gxbFVRTlkKYVNRUlQzUTlWOXhsYzBuS3c1TGN3L1JXZ3ZMN0JPaEZtY0gyQWMrT2ZLQUFoRTVLTnBVeTgwV3pzaXh5ZFI2WApoVEVTbVVETHBsamFQREdLZWxCY0ZvT1BNYzRDeFNJUExMZ0p3dTNIT1FiYit6VS9pcUs4VVpKRDdwMDdycFN6CmdKMkRvTkVWSnBEU1h2N3ZwMERpZ1duRDN5dmw5RmxaVzJLOFZjZXp4ME5LRi9pbkplNFJ2cmQxcy9PSEhYQzQKczBmM2NWOVljZmYzL1BoV21RQ1NocmVwQm5WeUVMcXRpWTg5TkRqYVhlVVpsR1JwTTQ3Z21RQktaSCtnMXlENgorSlJTOGp6ZDlZS3VDZFF3NGR3REtPMjlyZ29OM1VrQ0F3RUFBVEFOQmdrcWhraUc5dzBCQVFzRkFBT0NBUUVBCk9CSXJkT0M2VWNCMFhOQ0U1VGx3NlJzM21EL1pGdmJmdk1YWmNCaGJGTlF3NHN0bmNyTGk2WEpKdVBVNVZHMDcKV05wQklUMzEyV1hLbHkxbGxmeWh4dGZRNGJ4SnI3SGUrLzRMeGU1bEVFTCtjK1BSQzVxMlhUby91VDExdHJjVQpiMnl1aDAvRzNZSnFqOUoyTDM4WEZ1Z0tDNGwrQUtoeTJsa1hLVUZaeVp0eGJMWWdLazFkb1RKeE1RVGl4dWxuCjNVOS9MdTlEMDRpQW9LS1RYMDgrZG51MXpTd0Rhd251UHoyN0lWOGdLNW5GN1VNak1iNFB2cEZvaTRDYmNJOTYKd2dIMy9KL0hDTWJRVG5rMVcxMEQwMHJFcU1UNUVVTCtjZlVwbG5vT1RYV2YvK3NaaXZvSU1vTmhSdFB0czB0SAptazdaUGs0bzd1RGZGNURXSkpUZkx3PT0KLS0tLS1FTkQgQ0VSVElGSUNBVEUtLS0tLQo="),
+			},
+		}
+
+		fakeClientset = fake.NewSimpleClientset(fakeSecret, fakeCM)
 		prismEndpoint = credentials.NutanixPrismEndpoint{
 			Address:  ip,
 			Port:     9440,
@@ -91,6 +104,11 @@ var _ = Describe("Kubernetes Environment Provider", Ordered, func() {
 			CredentialRef: &credentials.NutanixCredentialReference{
 				Kind:      credentials.SecretKind,
 				Name:      secretName,
+				Namespace: secretNamespace,
+			},
+			AdditionalTrustBundle: &credentials.NutanixTrustBundleReference{
+				Kind:      credentials.NutanixTrustBundleKindConfigMap,
+				Name:      cmName,
 				Namespace: secretNamespace,
 			},
 		}
@@ -101,10 +119,16 @@ var _ = Describe("Kubernetes Environment Provider", Ordered, func() {
 		prov = NewProvider(prismEndpoint, secretInformer, cmInformer)
 	})
 	It("must be able to look up secret", func() {
-		_, err := fakeClientset.CoreV1().Secrets(secretNamespace).Get(context.TODO(),
-			secretName, metav1.GetOptions{})
+		_, err := fakeClientset.CoreV1().Secrets(secretNamespace).Get(context.TODO(), secretName, metav1.GetOptions{})
 		Expect(err).To(Succeed())
+
 		_, err = secretInformer.Lister().Secrets(secretNamespace).Get(secretName)
+		Expect(err).To(Succeed())
+
+		_, err = fakeClientset.CoreV1().ConfigMaps(secretNamespace).Get(context.TODO(), cmName, metav1.GetOptions{})
+		Expect(err).To(Succeed())
+
+		_, err = cmInformer.Lister().ConfigMaps(secretNamespace).Get(cmName)
 		Expect(err).To(Succeed())
 	})
 	It("must get management endpoint", func() {
@@ -116,8 +140,9 @@ var _ = Describe("Kubernetes Environment Provider", Ordered, func() {
 				Username: username,
 				Password: password,
 			},
-			Insecure: true,
-			Address:  addr,
+			Insecure:              true,
+			Address:               addr,
+			AdditionalTrustBundle: "-----BEGIN CERTIFICATE-----\nMIICrDCCAZQCCQCNlxtuk8/zJzANBgkqhkiG9w0BAQsFADAXMRUwEwYDVQQDDAxh\nZG1pc3Npb25fY2EwIBcNMjIxMTE4MTUwMzExWhgPMjI5NjA5MDIxNTAzMTFaMBcx\nFTATBgNVBAMMDGFkbWlzc2lvbl9jYTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCC\nAQoCggEBAKrM6xVwelji7oa58hsL7PDYKA8qRxLqXsbACzHuvGqETBKfSH1lUQNY\naSQRT3Q9V9xlc0nKw5Lcw/RWgvL7BOhFmcH2Ac+OfKAAhE5KNpUy80WzsixydR6X\nhTESmUDLpljaPDGKelBcFoOPMc4CxSIPLLgJwu3HOQbb+zU/iqK8UZJD7p07rpSz\ngJ2DoNEVJpDSXv7vp0DigWnD3yvl9FlZW2K8Vcezx0NKF/inJe4Rvrd1s/OHHXC4\ns0f3cV9Ycff3/PhWmQCShrepBnVyELqtiY89NDjaXeUZlGRpM47gmQBKZH+g1yD6\n+JRS8jzd9YKuCdQw4dwDKO29rgoN3UkCAwEAATANBgkqhkiG9w0BAQsFAAOCAQEA\nOBIrdOC6UcB0XNCE5Tlw6Rs3mD/ZFvbfvMXZcBhbFNQw4stncrLi6XJJuPU5VG07\nWNpBIT312WXKly1llfyhxtfQ4bxJr7He+/4Lxe5lEEL+c+PRC5q2XTo/uT11trcU\nb2yuh0/G3YJqj9J2L38XFugKC4l+AKhy2lkXKUFZyZtxbLYgKk1doTJxMQTixuln\n3U9/Lu9D04iAoKKTX08+dnu1zSwDawnuPz27IV8gK5nF7UMjMb4PvpFoi4CbcI96\nwgH3/J/HCMbQTnk1W10D00rEqMT5EUL+cfUplnoOTXWf/+sZivoIMoNhRtPts0tH\nmk7ZPk4o7uDfF5DWJJTfLw==\n-----END CERTIFICATE-----\n",
 		}))
 	})
 })
