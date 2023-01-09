d451f9076683938014a6051ce7406030096c51e7
deepakm-ntnx <deepak.muley@nutanix.com>
2023-01-09 12:53:35 -0800

    added asserts instead of Logf per review comment

--- karbon/karbon_cluster_registration_service_test.go
+++ karbon/karbon_cluster_registration_service_test.go
@@ -3,6 +3,7 @@ package karbon
 import (
 	"fmt"
 	"net/http"
+	"strings"
 	"testing"
 
 	"github.com/keploy/go-sdk/integrations/khttpclient"
@@ -12,34 +13,55 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func printK8sClusterRegistration(k8sClusterReg *K8sClusterRegistration) {
-	fmt.Printf("Type: K8sClusterRegistration\n")
-	fmt.Printf("|-Name: %s\n", *k8sClusterReg.Name)
-	fmt.Printf("|-Status: %s\n", k8sClusterReg.Status)
-	fmt.Printf("|-UUID: %s\n", k8sClusterReg.UUID)
-	// fmt.Printf("|-K8s Identity Name: %s\n", k8sClusterReg.Identity.Name)
-	// fmt.Printf("|-K8s Identity UUID: %s\n", *k8sClusterReg.Identity.UUID)
-	// fmt.Printf("|-K8s Identity Kind: %s\n", *k8sClusterReg.Identity.Kind)
-	fmt.Printf("|-Category Mapping: %#v\n", k8sClusterReg.CategoriesMapping)
+func validateK8sClusterRegistration(t *testing.T, k8sClusterReg *K8sClusterRegistration) {
+	assert.NotEmpty(t, *k8sClusterReg.Name)
+	assert.NotEmpty(t, k8sClusterReg.Status)
+	assert.NotEmpty(t, k8sClusterReg.UUID)
+	// TODO(deepakm-ntnx) enable following code once the crash is fixed
+	// assert.NotEmpty(t, k8sClusterReg.Identity.Name)
+	// assert.NotEmpty(t, *k8sClusterReg.Identity.Kind)
+	// assert.NotEmpty(t, *k8sClusterReg.Identity.UUID)
+	assert.NotEmpty(t, k8sClusterReg.CategoriesMapping)
+	assert.NotZero(t, k8sClusterReg.CategoriesMapping)
 }
 
-func printK8sClusterRegistrationDeleteResponse(delResp *K8sClusterRegistrationDeleteResponse) {
-	fmt.Printf("Type: K8sClusterRegistrationDeleteResponse\n")
-	fmt.Printf("|-Cluster Name: %s\n", delResp.ClusterName)
-	fmt.Printf("|-Cluster UUID: %s\n", delResp.ClusterUUID)
-	fmt.Printf("|-Task UUID: %s\n", *delResp.TaskUUID)
+func validateK8sClusterRegistrationGetResponse(t *testing.T, expected_k8s_cluster_name, expected_k8s_cluster_uuid string,
+	responseGetReg *K8sClusterRegistration) {
+	assert.NotEmpty(t, *responseGetReg.Name)
+	assert.Equal(t, expected_k8s_cluster_name, *responseGetReg.Name)
+	assert.NotEmpty(t, responseGetReg.Status)
+	assert.NotEmpty(t, responseGetReg.UUID)
+	assert.Equal(t, expected_k8s_cluster_uuid, responseGetReg.UUID)
+	// TODO(deepakm-ntnx) enable following code once the crash is fixed
+	// assert.NotEmpty(t, responseGetReg.Identity.Name)
+	// assert.NotEmpty(t, *responseGetReg.Identity.Kind)
+	// assert.NotEmpty(t, *responseGetReg.Identity.UUID)
+	// TODO(deepakm-ntnx) Currently API still allows to pass empty mappings. internal tracker filed
+	//assert.NotZero(t, len(responseGetReg.CategoriesMapping))
 }
 
-func printK8sClusterRegistrationCreateResponse(createResp *K8sCreateClusterRegistrationResponse) {
-	fmt.Printf("Type: K8sCreateClusterRegistrationResponse\n")
-	fmt.Printf("|-Cluster Name: %s\n", *createResp.ClusterName)
-	fmt.Printf("|-Cluster UUID: %s\n", *createResp.ClusterUUID)
-	fmt.Printf("|-Task UUID: %s\n", *createResp.TaskUUID)
+func validateK8sClusterRegistrationDeleteResponse(t *testing.T, expected_k8s_cluster_name, expected_k8s_cluster_uuid string,
+	delResp *K8sClusterRegistrationDeleteResponse) {
+	// TODO(deepakm-ntnx) currently following fails. internal tracker is filed
+	// assert.NotEmpty(t, delResp.ClusterName)
+	// assert.Equal(t, expected_k8s_cluster_name, delResp.ClusterName)
+	assert.NotEmpty(t, delResp.ClusterUUID)
+	assert.Equal(t, expected_k8s_cluster_uuid, delResp.ClusterUUID)
+	assert.NotEmpty(t, *delResp.TaskUUID)
 }
 
-func printK8sClusterRegistrationList(clusterRegList *K8sClusterRegistrationList) {
+func validateK8sClusterRegistrationCreateResponse(t *testing.T, expected_k8s_cluster_name, expected_k8s_cluster_uuid string,
+	createResp *K8sCreateClusterRegistrationResponse) {
+	assert.NotEmpty(t, createResp.ClusterName)
+	assert.Equal(t, expected_k8s_cluster_name, *createResp.ClusterName)
+	assert.NotEmpty(t, createResp.ClusterUUID)
+	assert.Equal(t, expected_k8s_cluster_uuid, *createResp.ClusterUUID)
+	assert.NotEmpty(t, *createResp.TaskUUID)
+}
+
+func validateK8sClusterRegistrationList(t *testing.T, clusterRegList *K8sClusterRegistrationList) {
 	for _, k8sRegistration := range *clusterRegList {
-		printK8sClusterRegistration(k8sRegistration)
+		validateK8sClusterRegistration(t, k8sRegistration)
 	}
 }
 
@@ -54,8 +76,8 @@ func TestKarbonCreateClusterRegistration(t *testing.T) {
 		Name: t.Name(),
 	})
 
-	test_cluster_name := "cluster1"
-	test_cluster_uuid := "634F5852-CE1B-465C-A95A-9B8DFFBDDE42"
+	test_cluster_name := strings.ToLower("cluster1")
+	test_cluster_uuid := strings.ToLower("634F5852-CE1B-465C-A95A-9B8DFFBDDE42")
 	test_category_mapping := map[string]string{
 		fmt.Sprintf("kubernetes-io-cluster-%s", test_cluster_name): "owned",
 		"KubernetesClusterName": test_cluster_name,
@@ -68,12 +90,12 @@ func TestKarbonCreateClusterRegistration(t *testing.T) {
 	responseGetReg, err := nkeClient.ClusterRegistrationOperations.GetK8sRegistration(kctx, test_cluster_uuid)
 	if err == nil {
 		fmt.Println("Get Cluster registration exists")
-		printK8sClusterRegistration(responseGetReg)
+		validateK8sClusterRegistrationGetResponse(t, test_cluster_name, test_cluster_uuid, responseGetReg)
 		// Registration exists. delete it so that we can create it
 		fmt.Println("Delete Cluster registration")
 		responseDelReg, err := nkeClient.ClusterRegistrationOperations.DeleteK8sRegistration(kctx, test_cluster_uuid)
 		assert.NoError(t, err)
-		printK8sClusterRegistrationDeleteResponse(responseDelReg)
+		validateK8sClusterRegistrationDeleteResponse(t, test_cluster_name, test_cluster_uuid, responseDelReg)
 	}
 
 	fmt.Println("Create Cluster registration")
@@ -87,7 +109,7 @@ func TestKarbonCreateClusterRegistration(t *testing.T) {
 	// returns type K8sCreateClusterRegistrationResponse
 	responseCreateReg, err := nkeClient.ClusterRegistrationOperations.CreateK8sRegistration(kctx, createRequest)
 	assert.NoError(t, err)
-	printK8sClusterRegistrationCreateResponse(responseCreateReg)
+	validateK8sClusterRegistrationCreateResponse(t, test_cluster_name, test_cluster_uuid, responseCreateReg)
 
 	// TODO get task uuid status
 }
@@ -106,5 +128,5 @@ func TestKarbonGetK8sRegistrationList(t *testing.T) {
 	// returns type K8sCreateClusterRegistrationResponse
 	response, err := nkeClient.ClusterRegistrationOperations.GetK8sRegistrationList(kctx)
 	assert.NoError(t, err)
-	printK8sClusterRegistrationList(response)
+	validateK8sClusterRegistrationList(t, response)
 }
