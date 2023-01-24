88c2dd23a27e4ac207eff9c730d33ef2bbd9f467
deepakm-ntnx <deepak.muley@nutanix.com>
2023-01-24 09:01:27 -0800

    added addonsetinfo test

--- karbon/karbon_cluster_registration_service_test.go
+++ karbon/karbon_cluster_registration_service_test.go
@@ -262,6 +262,93 @@ func TestKarbonCreateClusterRegistrationAndSetInfo(t *testing.T) {
 	}
 }
 
+func TestKarbonCreateClusterRegistrationAndAddonSetInfo(t *testing.T) {
+	interceptor := khttpclient.NewInterceptor(http.DefaultTransport)
+	creds := testhelpers.CredentialsFromEnvironment(t)
+	nkeClient, err := NewKarbonAPIClient(creds, WithRoundTripper(interceptor))
+	require.NoError(t, err)
+
+	kctx := mock.NewContext(mock.Config{
+		Mode: keploy.MODE_TEST,
+		Name: t.Name(),
+	})
+
+	test_cluster_name := strings.ToLower("cluster1")
+	test_cluster_uuid := strings.ToLower("634F5852-CE1B-465C-A95A-9B8DFFBDDE42")
+	test_category_mapping := map[string]string{
+		fmt.Sprintf("kubernetes-io-cluster-%s", test_cluster_name): "owned",
+		"KubernetesClusterName": test_cluster_name,
+		"KubernetesClusterUUID": test_cluster_uuid,
+	}
+	test_metadata_apiversion := "v1.1.0"
+	test_metadata := &Metadata{APIVersion: &test_metadata_apiversion}
+
+	t.Log("Get Cluster registration")
+	responseGetReg, err := nkeClient.ClusterRegistrationOperations.GetK8sRegistration(kctx, test_cluster_uuid)
+	if err == nil {
+		t.Log("Get Cluster registration exists")
+		validateK8sClusterRegistrationGetResponse(t, test_cluster_name, test_cluster_uuid, responseGetReg)
+		// Registration exists. delete it so that we can create it
+		t.Log("Delete Cluster registration")
+		responseDelReg, err := nkeClient.ClusterRegistrationOperations.DeleteK8sRegistration(kctx, test_cluster_uuid)
+		assert.NoError(t, err)
+		validateK8sClusterRegistrationDeleteResponse(t, test_cluster_name, test_cluster_uuid, responseDelReg)
+	}
+
+	t.Log("Create Cluster registration")
+	createRequest := &K8sCreateClusterRegistrationRequest{
+		Name:              &test_cluster_name,
+		UUID:              test_cluster_uuid,
+		CategoriesMapping: test_category_mapping,
+		Metadata:          test_metadata,
+	}
+
+	// returns type K8sCreateClusterRegistrationResponse
+	responseCreateReg, err := nkeClient.ClusterRegistrationOperations.CreateK8sRegistration(kctx, createRequest)
+	assert.NoError(t, err)
+	validateK8sClusterRegistrationCreateResponse(t, test_cluster_name, test_cluster_uuid, responseCreateReg)
+
+	// TODO get task uuid status
+
+	t.Log("Update CSI Info")
+	test_addon_name := "CSI"
+	test_addon_version := "v3.x"
+	test_cluster_addon_info := &K8sClusterAddonInfo{
+		AddonName:    test_addon_name,
+		AddonVersion: test_addon_version,
+	}
+	updateAddonInfoRequest := &K8sUpdateClusterRegistrationAddonInfoRequest{
+		ClusterAddonInfo: test_cluster_addon_info,
+	}
+	responseUpdateAddonInfo, err := nkeClient.ClusterRegistrationOperations.UpdateK8sRegistrationAddonInfo(kctx, test_cluster_uuid, test_addon_name, updateAddonInfoRequest)
+	assert.NoError(t, err)
+	validateK8sClusterRegistrationAddonInfoResponse(t, test_cluster_name, test_cluster_uuid, responseUpdateAddonInfo)
+
+	t.Log("Update CNDS Info")
+	test_addon_name = "CNDS"
+	test_addon_version = "v1.x"
+	test_cluster_addon_info = &K8sClusterAddonInfo{
+		AddonName:    test_addon_name,
+		AddonVersion: test_addon_version,
+	}
+	updateAddonInfoRequest = &K8sUpdateClusterRegistrationAddonInfoRequest{
+		ClusterAddonInfo: test_cluster_addon_info,
+	}
+	responseUpdateAddonInfo, err = nkeClient.ClusterRegistrationOperations.UpdateK8sRegistrationAddonInfo(kctx, test_cluster_uuid, test_addon_name, updateAddonInfoRequest)
+	assert.NoError(t, err)
+	validateK8sClusterRegistrationAddonInfoResponse(t, test_cluster_name, test_cluster_uuid, responseUpdateAddonInfo)
+
+	t.Log("Get Cluster registration")
+	responseGetReg, err = nkeClient.ClusterRegistrationOperations.GetK8sRegistration(kctx, test_cluster_uuid)
+	assert.NoError(t, err)
+	validateK8sClusterRegistrationGetResponseWithAddonInfo(t, test_cluster_name, test_cluster_uuid, test_cluster_addon_info, responseGetReg)
+
+	t.Log("Delete Cluster registration")
+	responseDelReg, err := nkeClient.ClusterRegistrationOperations.DeleteK8sRegistration(kctx, test_cluster_uuid)
+	assert.NoError(t, err)
+	validateK8sClusterRegistrationDeleteResponse(t, test_cluster_name, test_cluster_uuid, responseDelReg)
+}
+
 func TestKarbonGetK8sRegistrationList(t *testing.T) {
 	interceptor := khttpclient.NewInterceptor(http.DefaultTransport)
 	creds := testhelpers.CredentialsFromEnvironment(t)
