f7c9ee2c7d8f3bb46898cfa44c3eea8ed51d75e0
zihui.liu <zihui.liu@nutanix.com>
2023-04-24 20:21:50 -0700

    Update to add the apis for metrics call

--- karbon/karbon_cluster_registration_service.go
+++ karbon/karbon_cluster_registration_service.go
@@ -105,3 +105,31 @@ func (op ClusterRegistrationOperations) UpdateK8sRegistrationAddonInfo(ctx conte
 	}
 	return karbonClusterActionResponse, nil
 }
+
+// UpdateK8sRegistrationInfo updates k8s info
+func (op ClusterRegistrationOperations) UpdateK8sRegistrationMetrics(ctx context.Context, k8sClusterUUID string, updateInfoRequest *K8sUpdateClusterRegistrationMetricsRequest) (*K8sUpdateClusterRegistrationMetricsResponse, error) {
+	path := "/v1-alpha.1/k8s/cluster-registrations/" + k8sClusterUUID + "/metrics"
+	req, err := op.httpClient.NewRequest(http.MethodPost, path, updateInfoRequest)
+	if err != nil {
+		return nil, err
+	}
+	karbonClusterActionResponse := new(K8sUpdateClusterRegistrationMetricsResponse)
+	if err := op.httpClient.Do(ctx, req, karbonClusterActionResponse); err != nil {
+		return nil, err
+	}
+	return karbonClusterActionResponse, nil
+}
+
+// UpdateK8sRegistrationAddonInfo updates k8s info
+func (op ClusterRegistrationOperations) UpdateK8sRegistrationAddonMetrics(ctx context.Context, k8sClusterUUID, addonName string, updateAddonInfoRequest *K8sUpdateClusterRegistrationAddonMetricsRequest) (*K8sUpdateClusterRegistrationAddonMetricsResponse, error) {
+	path := "/v1-alpha.1/k8s/cluster-registrations/" + k8sClusterUUID + "/addons/" + addonName + "/metrics"
+	req, err := op.httpClient.NewRequest(http.MethodPost, path, updateAddonInfoRequest)
+	if err != nil {
+		return nil, err
+	}
+	karbonClusterActionResponse := new(K8sUpdateClusterRegistrationAddonMetricsResponse)
+	if err := op.httpClient.Do(ctx, req, karbonClusterActionResponse); err != nil {
+		return nil, err
+	}
+	return karbonClusterActionResponse, nil
+}
