4dfb94702e269dee3dd922b9040da01b50445f97
Abhay <abhay.aggrawal@nutanix.com>
2025-09-08 17:07:58 +0530

    Revert "Implement Clusters interface in Converged client v4"
    This reverts commit 15015138e4fb44419e4aa1838adbd3216b6e0baa.

--- converged_client/clusters.go
+++
@@ -1,9 +0,0 @@
-package convergedclient
-
-type Clusters[Cluster any] interface {
-	// Getter is the interface for Get operations.
-	Getter[Cluster]
-
-	// Lister is the interface for List operations.
-	Lister[Cluster]
-}
