ece8b5b13429568aba9cad2351ea6b6a5c06e8b6
Ilya Alekseyev <ilya.alekseyev@nutanix.com>
2025-09-09 19:34:24 +0200

    Improvements and add missed methods

--- converged_client/v4/storage_containers_test.go
+++ converged_client/v4/storage_containers_test.go
@@ -124,7 +124,8 @@ func TestStorageContainersService_NewIterator(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			iterator := service.NewIterator(tt.opts...)
+			ctx := context.Background()
+			iterator := service.NewIterator(ctx, tt.opts...)
 			assert.Nil(t, iterator)
 		})
 	}
@@ -148,7 +149,7 @@ func TestStorageContainersInterface(t *testing.T) {
 	assert.Error(t, err)
 
 	// Test NewIterator method
-	iterator := service.NewIterator()
+	iterator := service.NewIterator(ctx)
 	assert.Nil(t, iterator)
 }
 
@@ -190,7 +191,7 @@ func TestStorageContainersService_ClientDependency(t *testing.T) {
 	assert.Error(t, err)
 	assert.Contains(t, err.Error(), "not implemented")
 
-	iterator := service.NewIterator()
+	iterator := service.NewIterator(ctx)
 	assert.Nil(t, iterator)
 }
 
@@ -268,7 +269,7 @@ func TestStorageContainersService_ODataOptions(t *testing.T) {
 	})
 
 	t.Run("complex options for NewIterator", func(t *testing.T) {
-		iterator := service.NewIterator(complexOptions...)
+		iterator := service.NewIterator(ctx, complexOptions...)
 		assert.Nil(t, iterator)
 	})
 }
