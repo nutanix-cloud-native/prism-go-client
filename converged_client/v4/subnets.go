3c5da1d4d852ee4bbdd093e331cf8dad5b699110
atulv7 <atul.verma@nutanix.com>
2025-09-11 09:31:37 +0530

    feat: SubnetsService methods and tests

--- converged_client/v4/subnets.go
+++ converged_client/v4/subnets.go
@@ -2,7 +2,6 @@ package v4
 
 import (
 	"context"
-	"fmt"
 
 	converged "github.com/nutanix-cloud-native/prism-go-client/converged_client"
 	v4prismGoClient "github.com/nutanix-cloud-native/prism-go-client/v4"
@@ -21,15 +20,47 @@ func NewSubnetsService(client *v4prismGoClient.Client) *SubnetsService {
 
 // Get returns the subnet for the given UUID.
 func (s *SubnetsService) Get(ctx context.Context, uuid string) (*subnetModels.Subnet, error) {
-	return nil, fmt.Errorf("not implemented")
+	return GenericGetEntity[*subnetModels.GetSubnetApiResponse, subnetModels.Subnet](
+		func() (*subnetModels.GetSubnetApiResponse, error) {
+			return s.client.SubnetsApiInstance.GetSubnetById(&uuid)
+		},
+		"subnet",
+	)
 }
 
 // List returns a list of subnets.
 func (s *SubnetsService) List(ctx context.Context, opts ...converged.ODataOption) ([]subnetModels.Subnet, error) {
-	return nil, fmt.Errorf("not implemented")
+	return GenericListEntities[*subnetModels.ListSubnetsApiResponse, subnetModels.Subnet](
+		func(reqParams *V4ODataParams) (*subnetModels.ListSubnetsApiResponse, error) {
+			return s.client.SubnetsApiInstance.ListSubnets(
+				reqParams.Page,
+				reqParams.Limit,
+				reqParams.Filter,
+				reqParams.OrderBy,
+				reqParams.Expand,
+				reqParams.Select,
+			)
+		},
+		opts,
+		"subnets",
+	)
 }
 
 // NewIterator returns an iterator for listing subnets.
 func (s *SubnetsService) NewIterator(ctx context.Context, opts ...converged.ODataOption) converged.Iterator[subnetModels.Subnet] {
-	return nil
+	return GenericNewIterator[*subnetModels.ListSubnetsApiResponse, subnetModels.Subnet](
+		ctx,
+		func(ctx context.Context, reqParams *V4ODataParams) (*subnetModels.ListSubnetsApiResponse, error) {
+			return s.client.SubnetsApiInstance.ListSubnets(
+				reqParams.Page,
+				reqParams.Limit,
+				reqParams.Filter,
+				reqParams.OrderBy,
+				reqParams.Expand,
+				reqParams.Select,
+			)
+		},
+		opts,
+		"subnets",
+	)
 }
