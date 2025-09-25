9f4058ec0a6c8cdb10a8b1848843255de7ba41e5
atulv7 <atul.verma@nutanix.com>
2025-09-25 08:28:01 +0530

    feat: SubnetsService methods implementation

--- converged/v4/subnets.go
+++ converged/v4/subnets.go
@@ -2,7 +2,6 @@ package v4
 
 import (
 	"context"
-	"fmt"
 
 	converged "github.com/nutanix-cloud-native/prism-go-client/converged"
 	v4prismGoClient "github.com/nutanix-cloud-native/prism-go-client/v4"
@@ -10,7 +9,7 @@ import (
 	subnetModels "github.com/nutanix/ntnx-api-golang-clients/networking-go-client/v4/models/networking/v4/config"
 )
 
-// SubnetsService provides default "not implemented" implementation for all Subnets interface methods.
+// SubnetsService provides implementation for all Subnets interface methods.
 type SubnetsService struct {
 	client *v4prismGoClient.Client
 }
@@ -22,15 +21,47 @@ func NewSubnetsService(client *v4prismGoClient.Client) *SubnetsService {
 
 // Get returns the subnet for the given UUID.
 func (s *SubnetsService) Get(ctx context.Context, uuid string) (*subnetModels.Subnet, error) {
-	return nil, fmt.Errorf("not implemented")
+	return GenericGetEntity[*subnetModels.GetSubnetApiResponse, subnetModels.Subnet](
+		func() (*subnetModels.GetSubnetApiResponse, error) {
+			return s.client.SubnetsApiInstance.GetSubnetById(&uuid)
+		},
+		"subnet",
+	)
 }
 
 // List returns a list of subnets.
 func (s *SubnetsService) List(ctx context.Context, opts ...converged.ODataOption) ([]subnetModels.Subnet, error) {
-	return nil, fmt.Errorf("not implemented")
+	return GenericListEntities[*subnetModels.ListSubnetsApiResponse, subnetModels.Subnet](
+		func(reqParams *V4ODataParams) (*subnetModels.ListSubnetsApiResponse, error) {
+			return s.client.SubnetsApiInstance.ListSubnets(
+				reqParams.Page,
+				reqParams.Limit,
+				reqParams.Filter,
+				reqParams.OrderBy,
+				reqParams.Expand,
+				reqParams.Select,
+			)
+		},
+		opts,
+		"subnets",
+	)
 }
 
 // NewIterator returns an iterator for listing subnets.
 func (s *SubnetsService) NewIterator(ctx context.Context, opts ...converged.ODataOption) converged.Iterator[subnetModels.Subnet] {
-	return nil
+	return GenericNewIterator[*subnetModels.ListSubnetsApiResponse, subnetModels.Subnet](
+		ctx,
+		func(ctx context.Context, reqParams *V4ODataParams) (*subnetModels.ListSubnetsApiResponse, error) {
+			return s.client.SubnetsApiInstance.ListSubnets(
+				reqParams.Page,
+				reqParams.Limit,
+				reqParams.Filter,
+				reqParams.OrderBy,
+				reqParams.Expand,
+				reqParams.Select,
+			)
+		},
+		opts,
+		"subnets",
+	)
 }
